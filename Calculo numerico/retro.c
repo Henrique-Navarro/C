882bed5f60a9a7571dc9c85e17bccc3eaedcabf4
Henrique-Navarro <henriquenavarromorais@gmail.com>
2021-10-14 08:04:25 -0300

    calculo

--- Calculo numerico/retro.c
+++ Calculo numerico/retro.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void lerA(int **matrizA, int n)
+void lerA(double **matrizA, int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 1; j <= n; j++)
         {
-            scanf("%d", &matrizA[i][j]);
+            scanf("%lf", &matrizA[i][j]);
         }
     }
 }
-void imprimirA(int **matrizA, int n)
+void imprimirA(double **matrizA, int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 1; j <= n; j++)
         {
-            printf(" %d ", matrizA[i][j]);
+            printf(" %.3lf ", matrizA[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-void lerB(int *matrizB, int n)
+void lerB(double *matrizB, int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        scanf("%d", &matrizB[i]);
+        scanf("%lf", &matrizB[i]);
     }
 }
-void imprimirB(int *matrizB, int n)
+void imprimirB(double *matrizB, int n)
 {
-    printf("\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        printf(" %d \n", matrizB[i]);
+        printf(" %.3lf \n", matrizB[i]);
     }
     printf("\n");
 }
@@ -45,57 +45,48 @@ int main()
     int n;
     scanf("%d", &n);
 
-    int **matrizA;
-    matrizA = malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
+    double **matrizA;
+    matrizA = malloc(n * sizeof(double));
+    for (int i = 1; i <= n; i++)
     {
-        matrizA[i] = malloc(3 * sizeof(int));
+        matrizA[i] = malloc(n * sizeof(double));
     }
     lerA(matrizA, n);
-
-    int *matrizB;
-    matrizB = malloc(n * sizeof(int));
-    lerB(matrizB, n);
-
-    printf("\nA matriz ficou assim: \n");
+    printf("\nA matrizA ficou assim: \n");
     imprimirA(matrizA, n);
+
+    double *matrizB;
+    matrizB = malloc(n * sizeof(double));
+    lerB(matrizB, n);
+    printf("\nA matrizB ficou assim: \n");
     imprimirB(matrizB, n);
 
-    int *x;
-    x = malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
+    /*printf("\nA matrizA ficou assim: \n");
+    imprimirA(matrizA, n);
+
+    printf("\nA matrizB ficou assim: \n");
+    imprimirB(matrizB, n);*/
+
+    double *x;
+    x = malloc(n * sizeof(double));
+    for (int i = 1; i <= n; i++)
     {
         x[i] = 0;
     }
 
-    int soma;
-
-    for (int i = n - 1; i <= 0; i--)
+    double soma;
+    for (int i = n; i >= 1; i--)
     {
         soma = 0;
-        for (int j = n - 1; j == i; j--)
+        for (int j = (i + 1); j <= n; j++)
         {
-            soma = (matrizA[i][j] * x[i + 1]) + soma;
+            soma = soma + matrizA[i][j] * x[j];
         }
-        x[i] = matrizB[i] / (matrizA[i][i] - soma);
+        x[i] = (matrizB[i] - soma) / matrizA[i][i];
     }
-
-    //2z= 3
-    //matrizA[n-1][n-1]= matrizB[n-1];
-    /*int z= matrizB[n-1] / matrizA[n-1][n-1];
-    int y= (-(matrizA[n-2][n-1] * z) + matrizB[n-2]) / matrizB[n-2];
-    int x= ((-(matrizA[n-3][n-1] * z) + (matrizA[n-3][n-2] * y)) + matrizB[n-3]) / matrizB[n-3];*/
-
-    /*
-       [0]  [1]  [2]  
-[0]    1x + 1y + 1z = 2
-[1]    0x - 2y - 2z = -5
-[2]    0x + 0y + 1z = -1/2
-    */
-
-    //printf("%d %d %d", x,y,z);
-    for (int i = 0; i < n; i++)
+    printf("\nA matrizX ficou assim: \n");
+    for (int i = 1; i <= n; i++)
     {
-        printf("valor: %d\n", x[i]);
+        printf("%.3lf\n", x[i]);
     }
 }
